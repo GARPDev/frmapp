de97a9f59bff7a15b541effc0c898840d2376518
agarciaGIT <agarcia@echalk.com>
2014-01-28 17:09:46 -0500

    add media query for handheld

--- www/js/app.js
+++ www/js/app.js
@@ -74,3 +74,9 @@ phonecatApp.config(['$routeProvider','$anchorScrollProvider',
       });
 	  
   }]);
+
+  // Set User Agent 
+  var b = document.documentElement;
+  b.setAttribute('data-useragent',  navigator.userAgent);
+  b.setAttribute('data-platform', navigator.platform );
+  b.className += ((!!('ontouchstart' in window) || !!('onmsgesturechange' in window))?' touch':'');
