7708602cc20a35cd9714be8776247e6f47138983
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-06-09 16:17:30 -0400

    debug

--- www/js/services/map.js
+++ www/js/services/map.js
@@ -13,49 +13,37 @@ frmServices.factory('mapService', ['$resource','$http',
 
     mapService.displayMap=function(selector, address, callback) {
 
-      $('#map-debug').empty().text('In displayMap'); 
+      $('#map-debug').empty().text('In displayMap');
 
-      if(google === null || typeof google === "undefined") {
-        $('#map-debug').empty().text('No google');
-
+      if(googleXX === null || typeof googleXX === "undefined") {
+        $('#map-debug').empty().text('No Google');
       } else {
-
-        $('#map-debug').empty().text('Google:' + google); 
-
-        map = new google.maps.Map(document.getElementById(selector), mapOptions);
-
-        $('#map-debug').empty().text('Get Map');
-
-        geocoder = new google.maps.Geocoder();
-
-        $('#map-debug').empty().text('Get geocoder');
-        
-        geocoder.geocode( { 'address': address}, function(results, status) {
-
-          $('#map-debug').empty().text('Call geocoder');
-
-          mapService.status = status;
-
-          if (status == google.maps.GeocoderStatus.OK) {
-
-            $('#map-debug').empty().text('Call setCenter');
-
-            map.setCenter(results[0].geometry.location);
-
-            $('#map-debug').empty().text('New marker');
-
-            var marker = new google.maps.Marker({
-                map: map,
-                position: results[0].geometry.location
-            });
-            callback(null, status);
-          } else {
-            $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
-            callback(500, status);
-          }
-
-        });
+        $('#map-debug').empty().text('Yes Google');
       }
+
+      // map = new google.maps.Map(document.getElementById(selector), mapOptions);
+
+      // $('#map-debug').empty().text('Get Map');
+
+      // geocoder = new google.maps.Geocoder();
+      
+      // geocoder.geocode( { 'address': address}, function(results, status) {
+
+      //   mapService.status = status;
+
+      //   if (status == google.maps.GeocoderStatus.OK) {
+      //     map.setCenter(results[0].geometry.location);
+      //     var marker = new google.maps.Marker({
+      //         map: map,
+      //         position: results[0].geometry.location
+      //     });
+      //     callback(null, status);
+      //   } else {
+      //     $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
+      //     callback(500, status);
+      //   }
+
+      // });
     }
 
     return mapService;
