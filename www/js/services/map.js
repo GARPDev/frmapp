efcb9759bdf1acc37ef33d53404c3bb12b5f611d
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-06-09 17:32:03 -0400

    remvoe debug

--- www/js/services/map.js
+++ www/js/services/map.js
@@ -12,38 +12,26 @@ frmServices.factory('mapService', ['$resource','$http',
       };
 
     mapService.displayMap=function(selector, address, callback) {
-
-      $('#map-debug').empty().text('In displayMap');
-
-      if(typeof google === "undefined") {
-        $('#map-debug').empty().text('No Google');
-      } else {
-        $('#map-debug').empty().text('Yes Google');
-      }
-
-      // map = new google.maps.Map(document.getElementById(selector), mapOptions);
-
-      // $('#map-debug').empty().text('Get Map');
-
-      // geocoder = new google.maps.Geocoder();
+      map = new google.maps.Map(document.getElementById(selector), mapOptions);
+      geocoder = new google.maps.Geocoder();
       
-      // geocoder.geocode( { 'address': address}, function(results, status) {
+      geocoder.geocode( { 'address': address}, function(results, status) {
 
-      //   mapService.status = status;
+        mapService.status = status;
 
-      //   if (status == google.maps.GeocoderStatus.OK) {
-      //     map.setCenter(results[0].geometry.location);
-      //     var marker = new google.maps.Marker({
-      //         map: map,
-      //         position: results[0].geometry.location
-      //     });
-      //     callback(null, status);
-      //   } else {
-      //     $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
-      //     callback(500, status);
-      //   }
+        if (status == google.maps.GeocoderStatus.OK) {
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+              map: map,
+              position: results[0].geometry.location
+          });
+          callback(null, status);
+        } else {
+          $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
+          callback(500, status);
+        }
 
-      // });
+      });
     }
 
     return mapService;
