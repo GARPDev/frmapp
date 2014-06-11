c8e2df6a1baac84463d8c389d9b08347197100fc
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-06-11 11:11:10 -0400

    changes

--- www/js/services/map.js
+++ www/js/services/map.js
@@ -16,30 +16,31 @@ frmServices.factory('mapService', ['$resource','$http',
       if(typeof googleXX === "undefined") {
         $('#map-debug').text($('#map-debug').text()+'No Google');
         callback(500, 'No Google');
+      } else {
+        map = new google.maps.Map(document.getElementById(selector), mapOptions);
+        geocoder = new google.maps.Geocoder();      
+
+        $('#map-debug').text($('#map-debug').text()+'Created Google Objects');
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+
+          mapService.status = status;
+
+          if (status == google.maps.GeocoderStatus.OK) {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+            callback(null, status);
+          } else {
+            $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
+            callback(500, status);
+          }
+
+        });
       }
 
-      map = new google.maps.Map(document.getElementById(selector), mapOptions);
-      geocoder = new google.maps.Geocoder();      
-
-      $('#map-debug').text($('#map-debug').text()+'Created Google Objects');
-
-      geocoder.geocode( { 'address': address}, function(results, status) {
-
-        mapService.status = status;
-
-        if (status == google.maps.GeocoderStatus.OK) {
-          map.setCenter(results[0].geometry.location);
-          var marker = new google.maps.Marker({
-              map: map,
-              position: results[0].geometry.location
-          });
-          callback(null, status);
-        } else {
-          $('#'+selector).innerHTML = ' Geocode was not successful for the following reason: ' + status;
-          callback(500, status);
-        }
-
-      });
     }
 
     return mapService;
