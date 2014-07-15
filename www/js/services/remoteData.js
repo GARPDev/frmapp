3a5f331da3fcf5a485fdb45fc6486b629228e502
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-07-15 16:04:45 -0400

    s

--- www/js/services/remoteData.js
+++ www/js/services/remoteData.js
@@ -172,9 +172,9 @@ frmServices.factory('remoteDataService', ['$resource','$http','authenticationSer
 
               // Register Msg ID from Google GCM or Apple
               $http.post(url + '/frmApp/user/' + authenticationService.user.Id + '/registerMsg', {'gcmId':remoteDataService.userData.settings.msgId}).success(function(data){
-
+                console.log('Reg: ' + data);
               }).error(function(data, status, headers, config) {
-                
+                console.log('Reg Error: ' + status);
               });
 
             }
