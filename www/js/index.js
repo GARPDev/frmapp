1cceec1ed7f5139f6dba8dba6e41c29273de89ed
agarciaGIT <garcia.alberto.crespo@gmail.com>
2014-07-14 22:29:02 -0400

    setup client

--- www/js/index.js
+++ www/js/index.js
@@ -44,6 +44,10 @@ var app = {
         pictureSource=navigator.camera.PictureSourceType;
         destinationType=navigator.camera.DestinationType;
 
+        var pushNotification = window.plugins.pushNotification;
+        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"43874697608","ecb":"app.onNotificationGCM"});
+
+
 		/*
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
@@ -54,5 +58,41 @@ var app = {
 
         console.log('Received Event: ' + id);
 		*/
+    }, // result contains any message sent from the plugin call
+    successHandler: function(result) {
+        alert('Push Callback Success! Result = '+result)
+        document.getElementById("msg").innerHTML = "New text:" + result;
+
+    },
+    errorHandler:function(error) {
+        alert('Push Error: ' + error);
+    },
+    onNotificationGCM: function(e) {
+        switch( e.event )
+        {
+            case 'registered':
+                if ( e.regid.length > 0 )
+                {
+                    console.log("Regid " + e.regid);
+                    alert('registration id = '+e.regid);
+                    document.getElementById("msg").innerHTML = "XX: <input type='text' value='" + e.regid + "'>" + e.regid;
+
+                }
+            break;
+ 
+            case 'message':
+              // this is the actual push notification. its format depends on the data model from the push server
+              alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+              
+            break;
+ 
+            case 'error':
+              alert('GCM error = '+e.msg);
+            break;
+ 
+            default:
+              alert('An unknown GCM event has occurred');
+              break;
+        }
     }
 };
