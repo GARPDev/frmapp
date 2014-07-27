330f164228f49d4c294c6f48a96b793a3c27efe0
agarciaGIT <agarcia@echalk.com>
2014-07-27 14:44:42 -0400

    a

--- www/js/controllers/exam.js
+++ www/js/controllers/exam.js
@@ -32,6 +32,9 @@ frmControllers.controller('FRMExamCtrl', ['$scope','$timeout','$location','$sce'
       display_c(86501);
     }, 0);
 
+    $timeout(function() {
+      $scope.elapsedTime++;
+    }, 1000);
 
     $scope.exitExam = function() {
       $('body').removeClass("modal-open");
@@ -199,44 +202,6 @@ frmControllers.controller('FRMExamCtrl', ['$scope','$timeout','$location','$sce'
       }
       alert('Question has been flagged.');
     }
-
-    var display_ct = function() {
-        // Calculate the number of days left
-        var days=Math.floor(window.start / 86400);
-        // After deducting the days calculate the number of hours left
-        var hours = Math.floor((window.start - (days * 86400 ))/3600)
-        // After days and hours , how many minutes are left
-        var minutes = Math.floor((window.start - (days * 86400 ) - (hours *3600 ))/60)
-        // Finally how many seconds left after removing days, hours and minutes.
-        var secs = Math.floor((window.start - (days * 86400 ) - (hours *3600 ) - (minutes*60)))
-
-        var x = window.start + "(" + days + " Days " + hours + " Hours " + minutes + " Minutes and " + secs + " Secondes " + ")";
-
-
-        //document.getElementById('ct').innerHTML = x;
-
-        $scope.elapsedTime = secs;
-
-        window.start= window.start- 1;
-        tt=display_c(window.start);
-    }
-
-    function display_c(start){
-        window.start = parseFloat(start);
-        var end = 0 // change this to stop the counter at a higher value
-        var refresh=1000; // Refresh rate in milli seconds
-        if(window.start >= end ){
-            var mytime=setTimeout('display_ct()',refresh)
-        } else {
-            alert("Time Over ");
-        }
-    }
-
-    function stop() {
-        clearTimeout(mytime);
-    }
-
-
 
   }
 ]);
