6b8a9c8eeadd0956cb1760548a729b11e422ad17
agarciaGIT <agarcia@echalk.com>
2014-08-03 13:47:38 -0400

    a

--- www/js/controllers/examDay.js
+++ www/js/controllers/examDay.js
@@ -59,7 +59,7 @@ frmControllers.controller('FRMExamDayCtrl', ['$scope','$timeout','$location','ex
             break;
         }
       }
-      alert("in1");
+      alert("in2");
 
       var startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
       var endDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
@@ -69,10 +69,35 @@ frmControllers.controller('FRMExamDayCtrl', ['$scope','$timeout','$location','ex
       alert("in3");
 
       try {
-        var cordova = window.plugins.calendar;  
-        alert("in4");
-        if(typeof cordova != "undefined") {
-          alert("done!!");
+        if(typeof window.plugins.calendar != "undefined") {
+          alert("in4");      
+          var startDate = new Date(2014,2,15,18,30,0,0,0); // beware: month 0 = january, 11 = december
+          var endDate = new Date(2014,2,15,19,30,0,0,0);
+          var title = "My nice event";
+          var location = "Home";
+          var notes = "Some notes about this event.";
+          var success = function(message) { alert("Success: " + JSON.stringify(message)); };
+          var error = function(message) { alert("Error: " + message); };
+          alert("in5"); 
+          // create a calendar (iOS only for now)
+          var calendarName = "GARP";
+          window.plugins.calendar.createCalendar(calendarName,success,error);
+
+          alert("in6"); 
+
+          // if you want to create a calendar with a specific color, pass in a JS object like this:
+          var createCalOptions = window.plugins.calendar.getCreateCalendarOptions();
+          createCalOptions.calendarName = "My Cal Name";
+          createCalOptions.calendarColor = "#FF0000"; // an optional hex color (with the # char), default is null, so the OS picks a color
+          window.plugins.calendar.createCalendar(createCalOptions,success,error);
+          alert("in7"); 
+
+          // delete a calendar (iOS only for now)
+          //window.plugins.calendar.deleteCalendar(calendarName,success,error);
+
+          // create an event silently (on Android < 4 an interactive dialog is shown)
+          window.plugins.calendar.createEvent(title,location,notes,startDate,endDate,success,error);
+          alert("in8");
         }
       }
       catch(err) {
