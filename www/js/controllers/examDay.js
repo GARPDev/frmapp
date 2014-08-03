0b20ed74970d6c7190b38ae6ba25298752ba2c34
agarciaGIT <agarcia@echalk.com>
2014-08-03 11:46:20 -0400

    fix save to phone

--- www/js/controllers/examDay.js
+++ www/js/controllers/examDay.js
@@ -45,39 +45,33 @@ frmControllers.controller('FRMExamDayCtrl', ['$scope','$timeout','$location','ex
         switch(idx) {
           case -1:
             reminder.text = 'Download Exam Admission Ticket';
-            reminder.startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
             break;
           case -2:
             reminder.text = 'Obtian and register your current non-expired Government Issued Photo ID';
-            reminder.startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
             break;
           case -3:
             reminder.text = 'Verify or aquire an approved Calculator';
-            reminder.startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
             break;
           case -4:
             reminder.text = 'Bring #2 or Hard Black (HB) pencils only';
-            reminder.startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
             break;
         }
-
-
       }
 
+      if(defined(cordova)) {
 
-      if(typeof cordova === "undefined" || cordova === null) {
+        var startDate = new Date(remoteDataService.userData.registeredExam.registrations.records[0].Exam_Site__r.Exam__r.Exam_Date__c);
+        var endDate = new startDate;
+        var title = reminder.text;
 
-        $scope.reminderStatus = "Failure: cordova not defined";
+        var location = $scope.userData.registeredExam.address + " " + $scope.userData.registeredExam.city + ", " + $scope.userData.registeredExam.state + " " + $scope.userData.registeredExam.zip;    
+      mapService.displayMap('map-canvas',address);
 
-      } else {
-
-        var startDate = new Date("December 19, 2013 13:00:00");
-        var endDate = new Date("December 19, 2013 14:30:00");
-        var title = "FRM App Event";
-        var location = "Home";
-        var notes = "Some notes about this event.";
+        //var notes = "Some notes about this event.";
         var success = function(message) { 
+
           $scope.reminderStatus = "Success: " + message;
+          alert($scope.reminderStatus);
           //$scope.userSession.reminderAdded = true;
           remoteDataService.userSession.reminderAdded = true;
           remoteDataService.commitData();
