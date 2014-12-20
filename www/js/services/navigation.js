63fc183f772fa1bb41dc79a36317721e1dbdfce2
agarciaGIT <agarcia@echalk.com>
2014-12-20 11:47:24 -0500

    fixes

--- www/js/services/navigation.js
+++ www/js/services/navigation.js
@@ -1,22 +1,32 @@
-frmServices.factory('navigationService', ['$resource','$http',
-  function($resource, $http){
+frmServices.factory('navigationService', ['$resource','$http','$state','$stateParams',
+  function($resource, $http, $state, $stateParams){
 
 
     var navigationService = {};
+    navigationService.currentNav = null;
 
     navigationService.pageTransitionOut = function(view) {
-      $('.page-container').fadeOut(function() {
-        if(view !== null && typeof view !== "undefined") {
-          document.location.hash = '#!/' + view;
-        }
-      });
+
+      if(navigationService.currentNav == view) {
+        util.$state.transitionTo($state.current, $stateParams, {
+            reload: true,
+            inherit: false,
+            notify: true
+        });
+      } else {
+        navigationService.currentNav = view;
+        $('.page-container').fadeOut(function() {
+          if(view !== null && typeof view !== "undefined") {
+            document.location.hash = '#!/' + view;
+          }
+        });
+      }
     }
 
     navigationService.pageTransitionIn = function() {
       $('.page-container').fadeIn();
       //$('.page-container').show("slide", { direction: "left" }, 500); 
     }
-
 
     navigationService.changeView = function(view) {
       navigationService.pageTransitionOut(view);
