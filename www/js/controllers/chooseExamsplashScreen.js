8cfa671773a4950fa379c24bac24b6887c8279b6
Kiran Chitraju <kiran.chitraju@garp.com>
2016-09-29 12:06:32 -0400

    mid commit

--- www/js/controllers/chooseExamsplashScreen.js
+++ www/js/controllers/chooseExamsplashScreen.js
@@ -1,12 +1,16 @@
-frmControllers.controller('ChooseExamCtrl', ['$scope','$timeout','$location','remoteDataService','navigationService',
-  function($scope,$timeout,$location,remoteDataService,navigationService) {
+frmControllers.controller('ChooseExamCtrl', ['$scope','$timeout','$location','remoteDataService','navigationService', 'authenticationService',
+	function($scope,$timeout,$location,remoteDataService,navigationService, authenticationService) {
 
+		$scope.selectExam = function(exam) {
+			if(exam == "erp"){
+				remoteDataService.exam = 'erp';
+				remoteDataService.EXAM = 'ERP';
+			} else if("frm"){
+				remoteDataService.exam = 'frm';
+				remoteDataService.EXAM = 'FRM';
+			};
+			navigationService.changeView('myaccount');
+		};
 
-    // console.log(remoteDataService);
-
-
-
-
-
-  }
-  ]);
+	}
+	]);
